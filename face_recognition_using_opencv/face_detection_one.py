55145df7f5dc20405afc0f66544b3add1fb5d56e
Ibne-Hassan2001 <63236119+Ibne-Hassan2001@users.noreply.github.com>
2021-06-07 06:51:22 +0600

    Update face_detection_one.py

--- face_recognition_using_opencv/face_detection_one.py
+++ face_recognition_using_opencv/face_detection_one.py
@@ -1,20 +1,17 @@
 import cv2
 
-capture = cv2.VideoCapture(0)
+face = cv2.VideoCapture(0)
 cascade_classifier = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 while True:
-    ret, videocapture = capture.read()
+    ret, videocapture = face.read()
     videocapture = cv2.cvtColor(videocapture,0)
     detections = cascade_classifier.detectMultiScale(videocapture)
     if len(detections) > 0:
-        (x,y,w,h) = detections[0]
-        videocapture = cv2.rectangle(videocapture,(x,y),(x+w,y+h),(255,0,0),3)
-
+        (x,y,a,b) = detections[0]
+        videocapture = cv2.rectangle(videocapture,(x,y),(x+a,y+b),(255,0,0),3)
     cv2.imshow('Face Window',videocapture)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+face.release()
 
-
-capture.release()
-#cv2.destroyAllWindows()
